6ef396f2245b0ca06e45549a4f25bcf5329219cb
ericoborgonove <borgonove@gmail.com>
2025-10-04 14:41:14 -0400

    fix(faleconosco): ajuste no modulo js

---
+++ arquivosAula/assets/js/meus_js/faleConosco.js
@@ -0,0 +1,95 @@
+import { estados } from '../../json/brasil.js';
+import { paises } from '../../json/mundo.js';
+
+function verEstados() {
+  const selectNaci = document.getElementById("select-naci");
+  const selectNatu = document.getElementById("select-natu");
+  const labelNatu = document.getElementById("label-natu");
+
+  // limpa antes de preencher
+  selectNatu.innerHTML = "";
+
+  if (selectNaci.value === 'BR') {
+    selectNatu.hidden = false;
+    labelNatu.hiden = false;
+
+    // adiciona uma linha vazia no select antes de carregar os estados
+    let option = document.createElement("option");
+    option.textContent = "";
+    option.value = "";
+    selectNatu.appendChild(option);
+
+    estados.forEach((estado) => {
+      let option = document.createElement("option");
+      option.textContent = estado.nome;
+      option.value = estado.sigla;
+      selectNatu.appendChild(option);
+    });
+  } else {
+    selectNatu.hidden = true;
+    labelNatu.hidden = true
+  }
+}
+
+function abrirOutros() {
+  const outros = document.getElementById("outros");
+  const generos = document.getElementsByName("genero");
+
+  if (generos[2].checked) {
+    outros.disabled = false;
+    outros.placeholder = "Digite o seu gênero";
+  } else {
+    outros.disabled = true;
+    outros.value = "";
+    outros.placeholder = "";
+  }
+}
+
+function togglePass() {
+  const senha1 = document.getElementById("senha1");
+  const senha2 = document.getElementById("senha2");
+  const btnVer = document.getElementById("btnVer");
+
+  if (senha1.type === "password" || senha2.type === "password") {
+    senha1.type = "text";
+    senha2.type = "text";
+    btnVer.innerHTML = "Desver";
+  } else {
+    senha1.type = "password";
+    senha2.type = "password";
+    btnVer.innerHTML = "Ver";
+  }
+}
+
+function checkPassword() {
+  const senha1 = document.getElementById("senha1");
+  const senha2 = document.getElementById("senha2");
+  const errorPass = document.getElementById("errorPass");
+
+  if (senha2.value !== "") {
+    if (senha1.value !== senha2.value) {
+      errorPass.innerHTML = "As senhas não conferem.";
+      errorPass.classList.remove("success");
+      errorPass.classList.add("error");
+      senha1.classList.add("errorInput");
+      senha2.classList.add("errorInput");
+    } else {
+      errorPass.innerHTML = "As senhas coincidem.";
+      senha1.classList.remove("errorInput");
+      senha2.classList.remove("errorInput");
+      errorPass.classList.remove("error");
+      errorPass.classList.add("success");
+    }
+  } else {
+    errorPass.innerHTML = "";
+  }
+}
+
+// Eventos
+document.getElementById("select-naci").addEventListener("change", verEstados);
+document.getElementById("btnVer").addEventListener("click", togglePass);
+document.querySelectorAll('input[name="genero"]').forEach(radio => {
+  radio.addEventListener("change", abrirOutros);
+});
+document.getElementById("senha1").addEventListener("input", checkPassword);
+document.getElementById("senha2").addEventListener("input", checkPassword);
